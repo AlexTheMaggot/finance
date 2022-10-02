b91558f25d266fd04109982874787f6bc8e66f6a
alexagapkin <alexthemaggot23@gmail.com>
2022-10-03 01:46:21 +0500

    add expense and income models

--- finance/api/models.py
+++ finance/api/models.py
@@ -1,1 +1,11 @@
 from django.db import models
+
+
+class ExpenseModel(models.Model):
+    name = models.CharField(max_length=200)
+    date = models.DateField()
+
+
+class IncomeModel(models.Model):
+    name = models.CharField(max_length=200)
+    date = models.DateField()
