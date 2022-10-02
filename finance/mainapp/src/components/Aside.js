62fd3a33a58effdf8ca58d3416a227321ba1be62
alexagapkin <alexthemaggot23@gmail.com>
2022-10-03 01:47:20 +0500

    add aside component

---
+++ finance/mainapp/src/components/Aside.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+
+
+export default class Aside extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="block">
+                <h3>Aside</h3>
+            </div>
+        );
+    }
+}
