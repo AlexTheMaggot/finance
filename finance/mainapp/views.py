42bf24e35d67e3bf308e6054e8f58d2c10c5c93e
alexagapkin <alexthemaggot23@gmail.com>
2022-10-03 01:44:14 +0500

    add index login and logout views

--- finance/mainapp/views.py
+++ finance/mainapp/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout as logout_view
 
 
 def index(request):
-    template = 'mainapp/index.html'
+    if request.user.is_authenticated or '/login/' in request.path:
+        template = 'mainapp/index.html'
+        return render(request, template)
+    else:
+        return redirect('mainapp:login')
+
+
+def login(request):
+    template = 'mainapp/auth.html'
     return render(request, template)
+
+
+def logout(requset):
+    logout_view(requset)
+    return redirect('mainapp:login')
