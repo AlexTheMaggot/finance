5eacde372cd0e28be70ce14d281a8dd25d90a0b3
alexagapkin <alexthemaggot23@gmail.com>
2022-10-03 01:46:50 +0500

    add header component

---
+++ finance/mainapp/src/components/Header.js
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+
+
+export default class Header extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="block">
+                <h3>Dashboard</h3>
+                <a href='/logout/' className='btn btn-danger'>Выход</a>
+            </div>
+        );
+    }
+}
