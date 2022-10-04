72e2c367db3f994d98410dadd14845e38df3e5cf
alexagapkin <alexthemaggot23@gmail.com>
2022-10-04 11:27:35 +0500

    add routes

--- finance/mainapp/src/components/App.js
+++ finance/mainapp/src/components/App.js
@@ -5,6 +5,7 @@ import {BrowserRouter, Route, Routes, Navigate, useHistory} from "react-router-d
 import Header from "./Header";
 import Content from "./Content";
 import Aside from "./Aside";
+import ExpendsCreate from "./ExpendsCreate";
 
 
 export default class App extends Component {
@@ -14,29 +15,34 @@ export default class App extends Component {
 
     render() {
         return (
-            <div className="container-fluid app">
-                <div className="row header">
-                    <div className="block__wrapper">
-                        <Header />
+            <BrowserRouter>
+                <div className="container-fluid app">
+                    <div className="row header">
+                        <div className="block__wrapper">
+                            <Header />
+                        </div>
                     </div>
-                </div>
-                <div className="row">
-                    <div className="col-3">
-                        <div className="row aside">
-                            <div className="block__wrapper">
-                                <Aside />
+                    <div className="row">
+                        <div className="col-12 col-lg-3">
+                            <div className="row aside">
+                                <div className="block__wrapper">
+                                    <Aside />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-12 col-lg-9">
+                            <div className="row content">
+                                <div className="block__wrapper">
+                                    <Routes>
+                                        <Route exact path='/' element={<Content />}></Route>
+                                        <Route exact path='/expends/create/' element={<ExpendsCreate />}></Route>
+                                    </Routes>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div className="col-9">
-                        <div className="row content">
-                            <div className="block__wrapper">
-                                <Content />
-                            </div>
-                        </div>
-                    </div>
                 </div>
-            </div>
+            </BrowserRouter>
         );
     }
 }
