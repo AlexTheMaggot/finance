7f61a1b40954b2a7d5072115c55f1b0415e17730
alexagapkin <alexthemaggot23@gmail.com>
2022-10-05 00:04:05 +0500

    fix imports

--- finance/mainapp/src/components/ExpendsCreate.js
+++ finance/mainapp/src/components/ExpendsCreate.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import {Navigate, redirect} from "react-router-dom";
+import {Navigate} from "react-router-dom";
 
 
 export default class ExpendsCreate extends Component {
