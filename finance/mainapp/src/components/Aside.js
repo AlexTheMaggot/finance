8511a578012f9597600d957739621ad7f5332925
alexagapkin <alexthemaggot23@gmail.com>
2022-10-04 11:27:12 +0500

    add links

--- finance/mainapp/src/components/Aside.js
+++ finance/mainapp/src/components/Aside.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import {Link} from "react-router-dom";
 
 
 export default class Aside extends Component {
@@ -10,6 +11,12 @@ export default class Aside extends Component {
         return (
             <div className="block">
                 <h3>Aside</h3>
+                <nav>
+                    <ul className='nav'>
+                        <li><Link to={'/'} className='nav__item btn btn-primary'>Главная</Link></li>
+                        <li><Link to={'/expends/create/'} className='nav__item btn btn-primary'>Новый расход</Link></li>
+                    </ul>
+                </nav>
             </div>
         );
     }
