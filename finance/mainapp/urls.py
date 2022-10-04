07fb44930eb41d164beb48685141a25c208dba49
alexagapkin <alexthemaggot23@gmail.com>
2022-10-04 11:39:37 +0500

    add incomes creating url

--- finance/mainapp/urls.py
+++ finance/mainapp/urls.py
@@ -6,6 +6,7 @@ app_name = 'mainapp'
 urlpatterns = [
     path('', views.index, name='index'),
     path('expends/create/', views.index, name='expends_create'),
+    path('incomes/create/', views.index, name='incomes_create'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
 ]
