50dde1b18ddbed9a0d84fe37d8584998f262c41c
alexagapkin <alexthemaggot23@gmail.com>
2022-10-05 00:03:33 +0500

    add expends list component

---
+++ finance/mainapp/src/components/ExpendsList.js
@@ -0,0 +1,73 @@
+import React, {Component} from 'react';
+
+export default class ExpendsList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [],
+        }
+    }
+    componentDidMount() {
+        fetch('/api/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                method: 'ExpendsList',
+            }),
+        }).then(data => data.json()).then((mydata) => {
+            if (mydata.result ===  'success') {
+                this.setState({data: mydata.content})
+            }
+        });
+    }
+
+    render () {
+        let data_1 = [
+            {
+                name: 123,
+                cost: 456,
+                data: 789,
+            },
+            {
+                name: 123,
+                cost: 456,
+                data: 789,
+            },
+            {
+                name: 123,
+                cost: 456,
+                data: 789,
+            },
+        ]
+        const data = this.state.data
+        return (
+            <div className="block">
+                <h3 className='mb-5'>Список расходов</h3>
+                <table className="table">
+                    <thead>
+                    <tr>
+                        <th scope="col">Дата</th>
+                        <th scope="col">Название</th>
+                        <th scope="col">Цена</th>
+                        <th scope="col"></th>
+                        <th scope="col"></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.data.map(item => (
+                            <tr key={item.id}>
+                                <td>{item.date}</td>
+                                <td>{item.name}</td>
+                                <td>{item.cost}</td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+}
