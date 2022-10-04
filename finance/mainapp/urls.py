a8bb3351801b781f67fc7d7fa51ff1f8ab2fa94d
alexagapkin <alexthemaggot23@gmail.com>
2022-10-04 11:25:28 +0500

    add expends creating url

--- finance/mainapp/urls.py
+++ finance/mainapp/urls.py
@@ -5,6 +5,7 @@ app_name = 'mainapp'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('expends/create/', views.index, name='expends_create'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
 ]
