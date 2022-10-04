c7fbc6c073ec607c6ed2c42b47f70bc6d465f46e
alexagapkin <alexthemaggot23@gmail.com>
2022-10-04 11:26:00 +0500

    add cost field to income and expends models

--- finance/api/models.py
+++ finance/api/models.py
@@ -4,8 +4,10 @@ from django.db import models
 class ExpenseModel(models.Model):
     name = models.CharField(max_length=200)
     date = models.DateField()
+    cost = models.IntegerField()
 
 
 class IncomeModel(models.Model):
     name = models.CharField(max_length=200)
     date = models.DateField()
+    cost = models.IntegerField()
