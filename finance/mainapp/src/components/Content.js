2e0db4c497dc1487e2952a7929f61d971286d2ac
alexagapkin <alexthemaggot23@gmail.com>
2022-10-06 00:52:15 +0500

    add get total balance

--- finance/mainapp/src/components/Content.js
+++ finance/mainapp/src/components/Content.js
@@ -4,12 +4,31 @@ import React, {Component} from "react";
 export default class Content extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            data: '',
+        }
     }
-
+    componentDidMount() {
+        fetch('/api/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                method: 'GetTotalBalance',
+            }),
+        }).then(data => data.json()).then((mydata) => {
+            if (mydata.result ===  'success') {
+                this.setState({data: mydata.content})
+            }
+        });
+    }
     render() {
         return (
             <div className="block">
-                <h3>Content</h3>
+                <h3>Баланс</h3>
+                <h3>{this.state.data.total_balance_uzs} UZS</h3>
+                <h3>{this.state.data.total_balance_usd} $</h3>
             </div>
         );
     }
