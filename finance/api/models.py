fc3411da442af11e159180da6429cc829ba42b36
alexagapkin <alexthemaggot23@gmail.com>
2022-10-05 21:15:12 +0500

    add currency field to models

--- finance/api/models.py
+++ finance/api/models.py
@@ -5,9 +5,11 @@ class ExpenseModel(models.Model):
     name = models.CharField(max_length=200)
     date = models.DateField()
     cost = models.IntegerField()
+    currency = models.CharField(max_length=200)
 
 
 class IncomeModel(models.Model):
     name = models.CharField(max_length=200)
     date = models.DateField()
     cost = models.IntegerField()
+    currency = models.CharField(max_length=200)
