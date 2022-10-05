5dbe8e02a0d8abd0165e8787b448670502741942
alexagapkin <alexthemaggot23@gmail.com>
2022-10-05 21:07:46 +0500

    change buttons color

--- finance/mainapp/src/components/Aside.js
+++ finance/mainapp/src/components/Aside.js
@@ -14,8 +14,8 @@ export default class Aside extends Component {
                 <nav>
                     <ul className='nav'>
                         <li><Link to={'/'} className='nav__item btn btn-primary'>Главная</Link></li>
-                        <li><Link to={'/expenses/create/'} className='nav__item btn btn-primary'>Новый расход</Link></li>
-                        <li><Link to={'/incomes/create/'} className='nav__item btn btn-primary'>Новый доход</Link></li>
+                        <li><Link to={'/expenses/create/'} className='nav__item btn btn-success'>Новый расход</Link></li>
+                        <li><Link to={'/incomes/create/'} className='nav__item btn btn-success'>Новый доход</Link></li>
                         <li><Link to={'/expenses/list/'} className='nav__item btn btn-primary'>Список расходов</Link></li>
                         <li><Link to={'/incomes/list/'} className='nav__item btn btn-primary'>Список доходов</Link></li>
                     </ul>
