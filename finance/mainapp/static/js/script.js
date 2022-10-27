a1e33e7d1878a32a5279ecb64b7818998e0688f0
alexthemaggot23@gmail.com <alexthemaggot23@gmail.com>
2022-10-27 12:43:32 +0500

    Finance v1.0.3
    - make PWA ready app

--- finance/mainapp/static/js/script.js
+++ finance/mainapp/static/js/script.js
@@ -8,3 +8,10 @@ $(window).on('load', function () {
         $('#preloader').css('display', 'none');
     }, 2500);
 })
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./sw.js')
+      .then(() => navigator.serviceWorker.ready.then((worker) => {
+        worker.sync.register('syncdata');
+      }))
+      .catch((err) => console.log(err));
+}
---
+++ public/sw.js
@@ -0,0 +1,31 @@
+const CACHE = 'network-or-cache-v1';
+const timeout = 300;
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE).then((cache) => cache.addAll([
+                '/static'
+            ])
+        ));
+});
+self.addEventListener('fetch', (event) => {
+    event.respondWith(fromNetwork(event.request, timeout)
+      .catch((err) => {
+          console.log(`Error: ${err.message()}`);
+          return fromCache(event.request);
+      }));
+});
+function fromNetwork(request, timeout) {
+    return new Promise((fulfill, reject) => {
+        var timeoutId = setTimeout(reject, timeout);
+        fetch(request).then((response) => {
+            clearTimeout(timeoutId);
+            fulfill(response);
+        }, reject);
+    });
+}
+function fromCache(request) {
+    return caches.open(CACHE).then((cache) =>
+        cache.match(request).then((matching) =>
+            matching || Promise.reject('no-match')
+        ));
+}
