e4efeba2c31b8b91303f12de31e8c17a61a4ea75
alexthemaggot23@gmail.com <alexthemaggot23@gmail.com>
2022-10-27 13:21:29 +0500

    test service worker

--- public/sw.js
+++ public/sw.js
@@ -1,31 +1,11 @@
-const CACHE = 'network-or-cache-v1';
-const timeout = 300;
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE).then((cache) => cache.addAll([
-                '/static'
-            ])
-        ));
+    console.log('Установлен');
 });
+
+self.addEventListener('activate', (event) => {
+    console.log('Активирован');
+});
+
 self.addEventListener('fetch', (event) => {
-    event.respondWith(fromNetwork(event.request, timeout)
-      .catch((err) => {
-          console.log(`Error: ${err.message()}`);
-          return fromCache(event.request);
-      }));
+    console.log('Происходит запрос на сервер');
 });
-function fromNetwork(request, timeout) {
-    return new Promise((fulfill, reject) => {
-        var timeoutId = setTimeout(reject, timeout);
-        fetch(request).then((response) => {
-            clearTimeout(timeoutId);
-            fulfill(response);
-        }, reject);
-    });
-}
-function fromCache(request) {
-    return caches.open(CACHE).then((cache) =>
-        cache.match(request).then((matching) =>
-            matching || Promise.reject('no-match')
-        ));
-}
